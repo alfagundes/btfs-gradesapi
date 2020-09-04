114d18d2ea69c57fae237784e0a81e2cd777ec75
alfagundes <alexandre.fagundes@neogrid.com>
2020-09-04 12:06:19 -0300

    Implementação

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     //origin: 'http://localhost:3131',
-    origin: 'https://btfs-gradesapp.herokuapp.com/',
+    origin: 'https://btfs-gradesapp.herokuapp.com/grade',
   })
 );
 
