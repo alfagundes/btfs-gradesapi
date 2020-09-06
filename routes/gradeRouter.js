f1f957004435426bf94b38b7c391aa5dc6a6fc2b
alfagundes <alexandre.fagundes@neogrid.com>
2020-09-06 19:53:16 -0300

    Ajuste controller

--- routes/gradeRouter.js
+++ routes/gradeRouter.js
@@ -3,11 +3,11 @@ import controller from '../controller/gradeController.js';
 
 const app = express();
 
-app.post('grade', controller.create);
-app.get('grade', controller.findAll);
-app.get('grade', controller.findOne);
-app.put('grade', controller.update);
-app.delete('grade', controller.removeAll);
-app.delete('grade', controller.remove);
+app.post('/grade', controller.create);
+app.get('/grade', controller.findAll);
+app.get('/grade/:id', controller.findOne);
+app.put('/grade/:id', controller.update);
+app.delete('/grade', controller.removeAll);
+app.delete('/grade/:id', controller.remove);
 
 export { app as gradeRouter };
